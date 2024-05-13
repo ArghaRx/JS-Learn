b361094db94673b22e92a9ef3e620e69741617be
Argha Ghosh <arghag889@gmail.com>
2024-05-13 21:23:23 +0530

    Day-1  ende

---
+++ day_1/05.comparasion.js
@@ -0,0 +1,21 @@
+
+
+// == only check the value
+// === chaeck the value as well as the datatype
+
+// console.log( 2 == 2);
+// console.log("2" == 2) // it gives true because string convert into number and then check the both values
+// console.log("2" === 2); // but here === ,,, firstone is string but 2nd is number though both value is same
+
+
+// ---------------------------------------------------------------------------------------------------
+
+// now all gives diff value so avoid the Process
+
+// console.log(null > 0);  //--> false
+console.log(null == 0);     // false
+console.log(null >= 0);
+
+console.log(undefined == 0);  //true
+console.log(undefined > 0);
+console.log(undefined < 0);
