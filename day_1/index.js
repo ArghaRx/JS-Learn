b4303f302fd915b9f2c81a5697afa0346137732b
Argha Ghosh <arghag889@gmail.com>
2024-05-13 11:40:22 +0530

    Day-1

---
+++ day_1/index.js
@@ -0,0 +1,28 @@
+// console.log('Hii version 4');
+// var a = 5;
+// var a = 8;
+// console.log(a);
+
+// let x = null;
+// console.log(x);
+
+
+// object
+let person = {
+    firstName : 'Argha',
+    age : 22,
+};
+
+console.log(person);
+
+
+// Array
+let array = ['Ram', 'Argha', 'Ghosh', 2, 2.5, true];
+console.log(array[0]);
+console.log(array[4]);
+console.log(array[9]);
+array[4] = 6.7777777; //update
+console.log(array[4]);
+
+
+
