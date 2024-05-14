15c7790f81e4e643eedd9d9c6a8bb1220c1f06fc
Argha Ghosh <arghag889@gmail.com>
2024-05-14 21:27:41 +0530

    09 maths and sum

---
+++ day_1/09.nums_and_Math.js
@@ -0,0 +1,33 @@
+var a = 100
+// console.log(a)
+
+var b = new Number(a)
+// console.log(b)
+
+// console.log(b.toString())
+
+// console.log(b.toFixed(2)) // fixed fixed the decimal value after . ----> 100.00 (op)
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);   // Object [Math] {}
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// rANDOM() GIVES RANDOM VALUE EVERY TIME but every it comes in range----- <0 to 1>
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+
