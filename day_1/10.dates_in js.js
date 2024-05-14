f69ef14d9271cdc28a20af65072d368b49036300
Argha Ghosh <arghag889@gmail.com>
2024-05-14 21:54:25 +0530

    10 -> date and time

---
+++ day_1/10.dates_in js.js
@@ -0,0 +1,23 @@
+
+
+//    Date
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString()) 
+
+// console.log(myDate.toISOString())     //2024-05-14T16:17:13.140Z
+
+// console.log(myDate.toJSON())
+
+// console.log(myDate.toLocaleString()) //14/5/2024, 9:47:13 pm
+
+// console.log(myDate.toLocaleDateString())     // 14/5/2024
+
+
+
+// ------------------
+// we can create our own Date
+let createDate = new Date(2024, 23, 5)
+console.log(createDate.toDateString())
