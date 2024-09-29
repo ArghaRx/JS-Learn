d92a3b49e982dc0dded93902b4981900839e5f9b
Argha Ghosh <arghag889@gmail.com>
2024-09-29 20:43:53 +0530

    splice.js in array

---
+++ Array/splice.js
@@ -0,0 +1,23 @@
+var arr = [1,2,3,6,5,4,8,9,5,2,1];
+
+// splice(starting ind,  number of elements to remove);
+
+arr.splice(4,2);
+console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+var arr2 = [1,2,3,4,5,6,7,8,9];
+
+// splice(starting ind,   number of elements to remove, elements to add);
+
+arr2.splice(5,2,10,12,14,15);
+console.log(arr2)
