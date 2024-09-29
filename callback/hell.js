f6e6473a088e8f943d282c5c98cf18ecb5023a26
Argha Ghosh <arghag889@gmail.com>
2024-09-29 13:37:23 +0530

    callback hell.js

---
+++ callback/hell.js
@@ -0,0 +1,29 @@
+function firstCallback(callback) {
+    setTimeout(() => {
+        console.log("First Callback");
+        callback();
+    }, 1000);
+}
+
+function secondCallback(callback) {
+    setTimeout(() => {
+        console.log("Second Callback");
+        callback();
+    }, 1000);
+}
+
+function thirdCallback(callback) {
+    setTimeout(() => {
+        console.log("Third Callback");
+        callback();
+    }, 1000);
+}
+
+// Callback hell
+firstCallback(() => {
+    secondCallback(() => {
+        thirdCallback(() => {
+            console.log("All callbacks executed");
+        });
+    });
+});
